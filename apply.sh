aef7a168f3eb09410e634bc1ba072bca37dfdff7
mamonaco1973 <mamonaco1973@gmail.com>
2025-04-18 19:42:02 +0000

    fix apply.sh

--- apply.sh
+++ apply.sh
@@ -13,3 +13,27 @@ terraform init
 terraform apply -auto-approve
 cd ..
 
+vault=$(az keyvault list --resource-group packer-rg --query "[?starts_with(name, 'packer-kv')].name | [0]" --output tsv)
+echo "NOTE: Key vault for secrets is $vault"
+secretsJson=$(az keyvault secret show --name packer-credentials --vault-name ${vault} --query value -o tsv)
+password=$(echo "$secretsJson" | jq -r '.password')
+ 
+
+cd 02-packer
+cd linux
+
+
+packer init .
+
+packer build \
+  -var="client_id=$ARM_CLIENT_ID" \
+  -var="client_secret=$ARM_CLIENT_SECRET" \
+  -var="subscription_id=$ARM_SUBSCRIPTION_ID" \
+  -var="tenant_id=$ARM_TENANT_ID" \
+  -var="password=$password" \
+  linux_image.pkr.hcl
+
+cd ..
+
+cd ..
+
